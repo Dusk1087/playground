c40bbcbce6ce02061164e193d5e0e6e068344c45
Reed Foster <133254542+Dusk1087@users.noreply.github.com>
2024-12-06 10:22:51 -0500

    'update'

--- slice.r
+++ slice.r
@@ -2,6 +2,7 @@ library(tidyverse)
 library(janitor)
 library(nycflights13)
 
+#group_by and summarize are used in order to calculate the average delay for each month.
 flights |> 
   group_by(month) |> 
   summarize(
@@ -9,6 +10,7 @@ flights |>
     n = n()
   )
 
+#prop is used to calculate the 10% of the total number of flights in each month.
 flights |> 
   group_by(month) |> 
   summarize(
@@ -16,15 +18,17 @@ flights |>
     prop = .1
   )
 
+#Slice is used to select the row with the smallest value of dep_delay in each group.
+#with_ties = FALSE is used to break ties.
 flights |> 
   group_by(dest) |> 
   slice_max(arr_delay, n=1, with_ties = FALSE) |>
   relocate(dest)
 
-# df |> slice_head(n = 1) takes the first row from each group.
-# df |> slice_tail(n = 1) takes the last row in each group.
-# df |> slice_min(x, n = 1) takes the row with the smallest value of column x.
-# df |> slice_max(x, n = 1) takes the row with the largest value of column x.
-# df |> slice_sample(n = 1) takes one random row.
+# slice_head(n = 1) takes the first row from each group.
+# slice_tail(n = 1) takes the last row in each group.
+# slice_min(x, n = 1) takes the row with the smallest value of column x.
+# slice_max(x, n = 1) takes the row with the largest value of column x.
+# slice_sample(n = 1) takes one random row.
 
 
