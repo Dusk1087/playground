25bffd09431ea75125df49d010ea4742d480873d
Reed Foster <133254542+Dusk1087@users.noreply.github.com>
2024-12-07 17:48:17 -0500

    updated

--- workflow.r
+++ workflow.r
@@ -1,4 +1,21 @@
 library(tidyverse)
 library(nycflights13)
 
-short_flights <- flights |> filter(air_time < 60)
+short_flights <- flights |> 
+  filter(air_time < 60)
+
+flights |> 
+  group_by(dest) |> 
+  summarize(
+    distance = mean(distance),
+    speed = mean(distance / air_time, na.rm = TRUE)
+  ) |> 
+  ggplot(aes(x = distance, y = speed)) +
+  geom_smooth(
+    method = "loess",
+    span = 0.5,
+    se = FALSE, #confidence internal
+    color = "white", 
+    linewidth = 4
+  ) +
+  geom_point()
